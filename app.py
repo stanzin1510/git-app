dc84f1be55adc59302873749f99d3b517cfbe656
Stanzin Angmo <stanzinangmo32925@gmail.com>
2024-01-28 10:15:17 +0530

    This commit will add image to the main page

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import streamlit as st
-st.title('CampusX')
+st.title('Thiksay Monastry')
+col1,col2 = st.columns(2)
+with col1:
+    st.image('thiksay monastry.png')
+with col2:
+    st.header('Thiksay Monastry is an Budddhist Monastry')
