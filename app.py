aaa66b300cf510c447ee10cb9ca0da1a5fdf3030
Stanzin Angmo <stanzinangmo32925@gmail.com>
2024-01-28 10:22:16 +0530

    Added mostly asked ques

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ with col1:
     st.image('thiksay monastry.png')
 with col2:
     st.header('Thiksay Monastry is an Budddhist Monastry')
+st.header('Mostly asked questions')
+st.subheader('Why is thiksay monastry famous')
+st.subheader('How old is thiksay Monastry')
+st.subheader('What is the entry fees for Thiksay Monastry')
